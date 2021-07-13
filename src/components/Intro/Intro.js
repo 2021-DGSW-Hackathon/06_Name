9ae98a4e8d1315443f2b3e2237b430e13217a906
pjn141414 <jina071049@gmail.com>
2021-07-13 21:55:56 +0900

    Asf

---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Introduce from '../../container/IntroContainer/Introduce';
+import './Intro.scss';
+
+const Intro = () => {
+  return (
+    <div className="Intro">
+      <div className="Intro-Content">
+
+      </div>
+    </div>
+  )
+}
+
+export default Intro;
---
+++ src/pages/IntroPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Intro from 'components/Intro/Intro';
+import Header from 'components/Header/Header';
+
+const IntroPage = () => {
+  return (
+    <>
+      <Header />
+      <Intro />
+    </>
+  )
+}
+
+export default IntroPage;
