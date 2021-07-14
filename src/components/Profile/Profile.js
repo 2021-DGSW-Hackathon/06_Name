e849447616f461b56ce85cfd6a8c717bb902c0fa
cjstmdgml <cjstmdgml5247@gmail.com>
2021-07-14 09:01:31 +0900

    afsdf

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,8 +7,8 @@ import RedLogo from "../../assets/RedLogo.png";
 const Profile = () => {
   return (
     <>
-      <div>
-        <img src={MainPic} alt="subPic" className="MainPic" />
+      <div className="Profile">
+        <img src={MainPic} alt="subPic" className="MainPic2" />
         <div className="ProfileForm">
           <img src={RedLogo} alt="subPic" className="RedLogo" />
           <span className="name">박지나</span>
@@ -18,29 +18,34 @@ const Profile = () => {
           </span>
           <button className="logoutBtn">로그아웃</button>
         </div>
-        <div className="ProfileMenu">
+        <nav className="ProfileMenu">
           <ul className="ProfileUl">
             <li className="ProfileLi">
-              <a className="ProfileA" href="">
+              <a className="ProfileA" href="#">
                 알림
               </a>
             </li>
             <li className="ProfileLi">
-              <a className="ProfileA" href="">
+              <a className="ProfileA" href="#">
                 MY 구독
               </a>
             </li>
             <li className="ProfileLi">
-              <a className="ProfileA" href="">
+              <a className="ProfileA" href="#">
                 캘린더
               </a>
             </li>
             <li className="ProfileLi">
-              <a className="ProfileA" href="">
+              <a className="ProfileA" href="#">
                 내 게시물
               </a>
             </li>
           </ul>
+        </nav>
+        <div className="underMenu">
+          <span className="uspan1">오늘의 일정이 없습니다</span> <br />
+          <span className="uspan2">알고 있는 많은 일정을 공유해보세요!</span> <br />
+          <button className="addBtn">일정 추가하기</button>
         </div>
       </div>
     </>
--- src/container/MainContainer/HotPostContainer.js
+++ src/container/MainContainer/HotPostContainer.js
@@ -1,11 +1,12 @@
 import ImageSlider from "components/ImageSlider/ImageSlider";
 import Login from "components/Login/Login";
+import Profile from "components/Profile/Profile";
 
 const HotPostContainer = () => {
   return (
     <>
       <ImageSlider />
-      <Login />
+      <Profile />
     </>
   );
 };
